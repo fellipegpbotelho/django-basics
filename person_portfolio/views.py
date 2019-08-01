c2a1a52a9e664820aa8fed028792af9284a4ddfc
Fellipe Geraldo Pereira Botelho <fellipegpbotelho@gmail.com>
2019-07-31 22:29:06 -0300

    finish user register

--- person_portfolio/views.py
+++ person_portfolio/views.py
@@ -7,13 +7,14 @@ from django.http import HttpResponse
 def sign_up(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
+        print(form.is_valid())
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=password)
             login(request, user)
-            return HttpResponse('A new user has been successfully registered')
+            return HttpResponse('A new user has been successfully registered!')
     else:
         form = UserCreationForm()
     return render(request, 'register.html', {'form': form})
