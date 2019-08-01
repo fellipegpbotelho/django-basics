d3fa4047c09f009ed250a514e3450be0a4425116
Fellipe Geraldo Pereira Botelho <fellipegpbotelho@gmail.com>
2019-07-31 23:27:49 -0300

    list users and logout user

--- person_portfolio/urls.py
+++ person_portfolio/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView
 
 from . import views
 
@@ -6,4 +7,8 @@ urlpatterns = [
     path('', views.sign_up, name='user_sign'),
     path('dashboard', views.user_dashboard, name='user_dashboard'),
     path('login', views.sign_in, name='login_user'),
+    path('logout', LogoutView.as_view(next_page='/'), name='logout'),
+    path('users', views.list_registered_users, name='system_users'),
+    path('activate/user/<int:user_id>', views.user_activate, name='activate_user'),
+    path('deactivate/user/<int:user_id>', views.user_deactivate, name='deactivate_user'),
 ]
--- person_portfolio/views.py
+++ person_portfolio/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login, authenticate
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 
 def sign_up(request):
@@ -41,3 +43,27 @@ def sign_in(request):
     else:
         msg.append('Invalid login credentials, try again!')
     return render(request, 'login.html', {'errors': msg})
+
+
+@login_required()
+def list_registered_users(request):
+    users = User.objects.all()
+    return render(request, 'users.html', {'users': users})
+
+
+@login_required()
+def user_deactivate(request, user_id):
+    user = User.objects.get(pk=user_id)
+    user.is_active = False
+    user.save()
+    messages.success(request, 'User account has been successfully deactivated!')
+    return redirect('system_users')
+
+
+@login_required()
+def user_activate(request, user_id):
+    user = User.objects.get(pk=user_id)
+    user.is_active = True
+    user.save()
+    messages.success(request, 'User account has been successfully activated!')
+    return redirect('system_users')
